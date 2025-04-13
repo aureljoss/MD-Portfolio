7f537264d81ad463e841799ec2fb2e25e7c424fe
Aurelie <josserand.n.aurelie@gmail.com>
2025-04-13 11:59:02 -0400

    Updates to simpler living room scene. Updates background

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,13 @@
 import { OrbitControls, useGLTF, useTexture, Center } from "@react-three/drei";
 
 export default function Experience() {
-  const { nodes } = useGLTF("./model/MD-Portfolio-Scene.glb");
+  const { nodes } = useGLTF("./model/Living Room Scene-MD.glb");
   const bakedTexture = useTexture("./model/baked-final-test.jpg");
   bakedTexture.flipY = false;
 
   return (
     <>
+      <color args={["#030202"]} attach="background" />
       <OrbitControls
         makeDefault
         autoRotateSpeed={-0.1}
@@ -14,12 +15,12 @@ export default function Experience() {
         enableZoom={true}
         minDistance={6}
         maxDistance={9}
-        enablePan={false}
+        // enablePan={false}
         dampingFactor={0.05}
         minPolarAngle={Math.PI / 3}
         maxPolarAngle={Math.PI / 2}
-        minAzimuthAngle={(Math.PI*2)/1.15}
-        maxAzimuthAngle={Math.PI*2}
+        // minAzimuthAngle={(Math.PI*2)/1.15}
+        // maxAzimuthAngle={Math.PI*2}
       />
 
       <Center>
